f664b6cb8b9a1e24191e65d832415b3031aa0d27
butlerfuqua <butlerfuqua@gmail.com>
2022-10-31 16:04:53 -0400

    Add login signuup pages

---
+++ pages/entry/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <NuxtLink to="/entry/signup">
+            Sign up
+        </NuxtLink>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'LoginPage',
+    layout: 'entry'
+})
+</script>
---
+++ pages/entry/signup.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Signup</h1>
+        <NuxtLink to="/entry/login">
+            Log in
+        </NuxtLink>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'LoginPage',
+    layout: 'entry'
+})
+</script>
