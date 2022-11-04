0ba2d85f42cf695186cf7483614e68525ebf5169
butlerfuqua <butlerfuqua@gmail.com>
2022-11-04 08:37:31 -0400

    Add loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div v-if="!errorMessage" class="p-2">
+  <div v-if="!errorMessage && !isLoading" class="p-2">
     <div v-for="testimony in testimonies">
       <p class="text-lg">{{ testimony.body }}</p>
     </div>
+  </div>
+  <div v-else-if="isLoading && !errorMessage" class="h-full">
+    <FullLoader />
   </div>
   <div v-else-if="errorMessage" class="p-2">
     <p class="text-lg">{{ errorMessage }}</p>
@@ -12,6 +15,8 @@
 <script lang="ts">
 import Vue from 'vue';
 import axios, { AxiosResponse } from 'axios';
+
+import FullLoader from '../components/layout/fullLoader.vue'
 
 export type Testimony = {
   // authorId: string
@@ -23,18 +28,20 @@ type Data = {
   orgLocation: null | string
   testimonies: null | Testimony[]
   errorMessage: null | string
+  isLoading: boolean
 }
 
 export default Vue.extend({
   name: 'IndexPage',
   layout: 'landing',
-  components: {},
+  components: { FullLoader },
   data(): Data {
     return {
       organization: null,
       orgLocation: null,
       testimonies: null,
       errorMessage: null,
+      isLoading: true
     }
   },
   methods: {
@@ -49,6 +56,7 @@ export default Vue.extend({
         this.errorMessage = `Sorry, there was an error.`;
         console.error(error);
       }
+      this.isLoading = false;
     }
   },
   async created() {
