47b4b79b88dfcf0116e88cd8ce7e8160481930c6
butlerfuqua <butlerfuqua@gmail.com>
2022-11-04 08:02:24 -0400

    Add env var to hey test

--- netlify/functions/hey.js
+++ netlify/functions/hey.js
@@ -1,4 +1,7 @@
 
+const { TB_SUPABASE_KEY: apiKey, TB_SUPABASE_URL: url, TB_SERVICE_ROLE: serviceRole } = process.env;
+
+
 export const handler = async (event) => {
 
     const { body } = event;
@@ -7,7 +10,7 @@ export const handler = async (event) => {
     try {
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'hey', organization, orgLocation }),
+            body: JSON.stringify({ message: 'hey', organization, orgLocation, url }),
         }
     } catch (error) {
         console.error(`Error #hey`, error)
