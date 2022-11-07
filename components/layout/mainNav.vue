9c5417f227f014dd4f38836d6838ec5b697ee92f
butlerfuqua <butlerfuqua@gmail.com>
2022-11-07 08:48:39 -0500

    Add nav toggle animation

--- components/layout/mainNav.vue
+++ components/layout/mainNav.vue
@@ -6,21 +6,30 @@
         <button @click="showNav = true" class="bg-blue-600 hover:bg-blue-400 text-white transition-all ease-in-out p-1">
             Menu
         </button>
-        <Transition>
-            <div v-if="showNav" class="fixed bg-purple-500 w-full h-full top-0 left-0">
-                <div class="flex justify-end">
-                    <button @click="showNav = false"
-                        class="bg-blue-600 hover:bg-blue-400 text-white transition-all ease-in-out p-1">
-                        Close
-                    </button>
+        <div id="mainNav" :class="showNav ? '' : 'hide-nav'" class="fixed bg-purple-500 w-full h-full top-0">
+            <div class="flex justify-end">
+                <button @click="showNav = false"
+                    class="bg-blue-600 hover:bg-blue-400 text-white transition-all ease-in-out p-1">
+                    Close
+                </button>
+            </div>
+            <div id="userInfo">
+                <div v-if="user" class="">
+                    {{ user.username }}
                 </div>
-                <div id="userInfo" class="text-white">
-                    <div v-if="user">
-                        {{ user.username }}
-                    </div>
+                <div v-else class="flex flex-col">
+                    <NuxtLink to="/entry/login">
+                        <button
+                            class="bg-blue-500 hover:bg-blue-400 transition-all eas-in-out text-white py-1 px-2 rounded shadow">Login</button>
+                    </NuxtLink>
+                    <NuxtLink to="/entry/signup">
+                        <button
+                            class="bg-yellow-300 hover:bg-yellow-200 transition-all eas-in-out text-black py-1 px-2 rounded shadow">Sign
+                            up</button>
+                    </NuxtLink>
                 </div>
             </div>
-        </Transition>
+        </div>
     </div>
 </template>
 
@@ -56,14 +65,16 @@ export default Vue.extend({
 })
 </script>
 
-<style>
-.v-enter-active,
-.v-leave-active {
-    transition: opacity 0.5s ease;
-}
+<style lang="scss">
+#mainNav {
 
-.v-enter-from,
-.v-leave-to {
-    opacity: 0;
+    transition: all .3s;
+    left: 0;
+    opacity: 1;
+
+    &.hide-nav {
+        left: 100vw;
+        opacity: 0;
+    }
 }
 </style>
